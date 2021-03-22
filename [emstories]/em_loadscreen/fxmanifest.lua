c7dc74a1bcb8125ed53dc0ab60ca0b7f91aa80f5
XJMLN <xjmln.programming@gmail.com>
2021-03-22 14:11:37 +0100

    loading screen

--- [emstories]/em_loadscreen/fxmanifest.lua
+++ [emstories]/em_loadscreen/fxmanifest.lua
@@ -1,3 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
+files {
+    'ui/index.html',
+    'ui/css/style.css',
+    'ui/images/logo.png',
+    'ui/css/fonts/Roboto-Light.woff2',
+}
+loadscreen 'ui/index.html'
