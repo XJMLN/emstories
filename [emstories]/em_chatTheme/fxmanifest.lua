f92d2d33eba70a6de4cea5545b9e2338fbfe4793
XJMLN <xjmln.programming@gmail.com>
2021-03-25 21:17:34 +0100

    chat theme

---
+++ [emstories]/em_chatTheme/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+file 'style.css'
+chat_theme 'emstories' {
+    styleSheet = 'style.css',
+    msgTemplates= {
+        default='<b>{0}</b><span> {1}<span>'
+    }
+}
