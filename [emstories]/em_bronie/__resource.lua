6fcac87d28b7ed5d775e3b33df85a0ddf5d71986
XJMLN <xjmln.programming@gmail.com>
2021-03-28 12:09:34 +0200

    add custom sound for weapons

---
+++ [emstories]/em_bronie/__resource.lua
@@ -0,0 +1,30 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+files {
+  'sound/audio/sfx/resident/explosions.awc',
+  'sound/audio/sfx/resident/weapons.awc',
+  'sound/audio/sfx/weapons_player/lmg_combat.awc',
+  'sound/audio/sfx/weapons_player/lmg_mg_player.awc',
+  'sound/audio/sfx/weapons_player/mgn_sml_am83_vera.awc',
+  'sound/audio/sfx/weapons_player/mgn_sml_am83_verb.awc',
+  'sound/audio/sfx/weapons_player/mgn_sml_sc__l.awc',
+  'sound/audio/sfx/weapons_player/ptl_50cal.awc',
+  'sound/audio/sfx/weapons_player/ptl_combat.awc',
+  'sound/audio/sfx/weapons_player/ptl_pistol.awc',
+  'sound/audio/sfx/weapons_player/ptl_px4.awc',
+  'sound/audio/sfx/weapons_player/ptl_rubber.awc',
+  'sound/audio/sfx/weapons_player/sht_bullpup.awc',
+  'sound/audio/sfx/weapons_player/sht_pump.awc',
+  'sound/audio/sfx/weapons_player/smg_micro.awc',
+  'sound/audio/sfx/weapons_player/smg_smg.awc',
+  'sound/audio/sfx/weapons_player/snp_heavy.awc',
+  'sound/audio/sfx/weapons_player/snp_rifle.awc',
+  'sound/audio/sfx/weapons_player/spl_grenade_player.awc',
+  'sound/audio/sfx/weapons_player/spl_minigun_player.awc',
+  'sound/audio/sfx/weapons_player/spl_prog_ar_player.awc',
+  'sound/audio/sfx/weapons_player/spl_railgun.awc',
+  'sound/audio/sfx/weapons_player/spl_rpg_player.awc',
+  'sound/audio/sfx/weapons_player/spl_tank_player.awc',
+}
+
+data_file 'AUDIO_WAVEPACK' 'sound/audio/sfx/resident'
+data_file 'AUDIO_WAVEPACK' 'sound/audio/sfx/weapons_player'
