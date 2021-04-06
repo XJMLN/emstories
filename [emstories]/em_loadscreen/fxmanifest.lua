106222875a7fff395e820df741b0eac096450339
XJMLN <xjmln.programming@gmail.com>
2021-04-06 10:29:49 +0200

    change audio volume

--- [emstories]/em_loadscreen/fxmanifest.lua
+++ [emstories]/em_loadscreen/fxmanifest.lua
@@ -6,6 +6,7 @@ files {
     'ui/css/style.css',
     'ui/images/logo.png',
     'ui/css/fonts/Roboto-Light.woff2',
+    'ui/music/music.mp3',
 }
 loadscreen 'ui/index.html'
 loadscreen_manual_shutdown 'yes'
