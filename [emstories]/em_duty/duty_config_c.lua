4d72635e45041f6192ae07eb8bbeab6951eb37a4
XJMLN <xjmln.programming@gmail.com>
2021-04-11 03:16:43 +0200

    Locations for LSMC

--- [emstories]/em_duty/duty_config_c.lua
+++ [emstories]/em_duty/duty_config_c.lua
@@ -1,11 +1,15 @@
 GARAGE_POS = {--[faction][department][pos]
     [3]={
         [1]={212.62,-1637.1,29.87,heading=319.45},
+    },
+    [2]={
+        [2]={358.67,-596.11,28.21,heading=159.71},
     }
 }
 vehicles = {}
 dutyMarkers = {
     {departmentID=1,factionID=3,pos={199.19,-1634.68,29.8}},--pos={199.18,-1634.69,29.8}},
+    {departmentID=2,factionID=2,pos={341.08,-580.84,28.79}},
 }
 skins = {}
 vehicles = {
