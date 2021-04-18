03b95e051f35c2d0b946c959d379d99e603ffa0a
XJMLN <xjmln.programming@gmail.com>
2021-04-18 11:54:11 +0200

    version text top left corner

--- [emstories]/em_gui/fxmanifest.lua
+++ [emstories]/em_gui/fxmanifest.lua
@@ -2,3 +2,4 @@ fx_version 'cerulean'
 game 'gta5'
 
 client_script 'playernames_c.lua'
+client_script 'version_c.lua'
---
+++ [emstories]/em_gui/version_c.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(0)
+
+            SetTextFont(0)
+            SetTextProportional(1)
+            SetTextScale(0.0, 0.3)
+            SetTextColour(255, 255, 255, 120)
+            SetTextDropshadow(0, 0, 0, 0, 255)
+            SetTextEdge(1, 0, 0, 0, 255)
+            SetTextDropShadow()
+            SetTextOutline()
+            SetTextEntry("STRING")
+            AddTextComponentString("Emergency Stories: Alpha")
+            DrawText(0.005, 0.005)
+end
+end)
