0c58f864066f295ad9d2474befb8ab4a75fc84c2
XJMLN <xjmln.programming@gmail.com>
2021-05-25 02:21:32 +0200

    fix in pd zones

--- [emstories]/em_pd_zones/zones.lua
+++ [emstories]/em_pd_zones/zones.lua
@@ -10,3 +10,9 @@ end
 
 RegisterNetEvent("zones:givePlayerXP")
 AddEventHandler("zones:givePlayerXP",zones_giveXP)
+
+RegisterCommand("setBag",function(source)
+    local plr = Player(source)
+    plr.state.departmentID = 5
+    print(plr.state.departmentID)
+end)
