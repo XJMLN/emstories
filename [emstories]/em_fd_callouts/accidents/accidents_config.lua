a5ba4cbc686fc5055eb1b71bb6290ebf12222ba2
XJMLN <xjmln.programming@gmail.com>
2021-06-06 17:07:43 +0200

    Delete all files with callouts - we created 1 main resource for them

--- [emstories]/em_fd_callouts/accidents/accidents_config.lua
+++
@@ -1,2 +0,0 @@
-ACCIDENTS_VEHICLES = {"primo"}
-ACCIDENTS_PEDS = {"s_f_y_airhostess_01"}
--- [emstories]/em_mc_callouts/ped/ped_callouts_config.lua
+++
@@ -1,8 +0,0 @@
-PED_MODELS = {
-    [4]={ -- mezczyzna
-        "a_m_y_cyclist_01",
-    },
-    [5]={-- kobieta
-        "u_f_y_comjane",
-    },
-}
